4ab9ed73fc074f4464897d3f652959ce3c2138c0
한재준 <gkswowns25@naver.com>
2025-07-17 02:46:24 +0900

    3rd class: Post join and 302 status, redirect

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -10,6 +10,7 @@ import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 
 public class RequestHandler extends Thread {
@@ -27,15 +28,15 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            String header = headerRead(in);
-            String[] token = header.split(" ");
+            header_and_length header = headerRead(in);
+            String[] token = header.getHeader().split(" ");
+            int length = header.getLength();
+
             String url = token[1];
 
             if(url.startsWith("/user/create")){
-                int index = url.indexOf("?");
-                String requestPath = url.substring(0, index);
-                String query = url.substring(index+1);
-                Map<String, String> params = HttpRequestUtils.parseQueryString(query);
+                String body = IOUtils.readData(header.getBufferedReader(), length);
+                Map<String, String> params = HttpRequestUtils.parseQueryString(body);
 
                 User user = new User(params.get("userId")
                         , params.get("password")
@@ -43,6 +44,11 @@ public class RequestHandler extends Thread {
                         , params.get("email"));
 
                 log.debug("User : {}", user);
+
+                DataOutputStream dos = new DataOutputStream(out);
+                byte[] response_body = Files.readAllBytes(new File("webapp" + "/index.html").toPath());
+                response302Header(dos, response_body.length);
+                responseBody(dos, response_body);
             }
             else {
                 DataOutputStream dos = new DataOutputStream(out);
@@ -67,6 +73,17 @@ public class RequestHandler extends Thread {
         }
     }
 
+    private void response302Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
             dos.write(body, 0, body.length);
@@ -76,11 +93,13 @@ public class RequestHandler extends Thread {
         }
     }
 
-    private String headerRead(InputStream in) throws IOException {
+    private header_and_length headerRead(InputStream in) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(in));
 
         String header = reader.readLine();
         log.debug("HTTP request Header : {}", header);
+
+        int length = 0;
 
         String line = ".";
 
@@ -89,9 +108,37 @@ public class RequestHandler extends Thread {
             if (line == null) {
                 break;
             }
+            if (line.startsWith("Content-Length")) {
+                String[] contLength_str = line.split(":");
+                length = Integer.parseInt(contLength_str[1].trim());
+            }
             log.debug("HTTP request : {}",line);
         }
 
-        return header;
+        return new header_and_length(length, header, reader);
+    }
+
+    static class header_and_length{
+        int length;
+        String header;
+        BufferedReader reader;
+
+        public header_and_length(int lenght, String header, BufferedReader reader){
+            this.header = header;
+            this.length = lenght;
+            this.reader = reader;
+        }
+
+        public int getLength(){
+            return length;
+        }
+
+        public String getHeader() {
+            return header;
+        }
+
+        public BufferedReader getBufferedReader() {
+            return reader;
+        }
     }
 }
